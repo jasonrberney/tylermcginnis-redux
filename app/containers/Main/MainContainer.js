20eed3461747f86b69128b6180d465411be5d3d6
Jason Berney <jasonrberney@gmail.com>
2018-01-02 11:08:44 -0800

    Creating the Home Route page.

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Navigation } from 'components'
+import { container, innerContainer } from './styles.css'
 
 const MainContainer = React.createClass({
     render () {
         return (
-            <div>
-                {this.props.children}
+            <div className={container}>
+                <Navigation isAuthed={true}/>
+                <div className={innerContainer}>
+                    {this.props.children}
+                </div>
             </div>
         )
     },
