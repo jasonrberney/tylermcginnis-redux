c2a475d2206481814b8851b730fac5fbdd276d2b
Jason Berney <jasonrberney@gmail.com>
2018-01-11 09:17:10 -0800

    Add components for individual Ducks and including likes.

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,3 +1,4 @@
+export DuckContainer from "./Duck/DuckContainer"
 export MainContainer from './Main/MainContainer'
 export HomeContainer from './Home/HomeContainer'
 export AuthenticateContainer from './Authenticate/AuthenticateContainer'
