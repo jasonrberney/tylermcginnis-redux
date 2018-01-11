cf602d03e69d4aeb8edc1e02e1d0ebd4dc7601e9
Jason Berney <jasonrberney@gmail.com>
2018-01-11 09:44:40 -0800

    Add Likes and remove likes handlers to connect with Firebase.

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -31,9 +31,30 @@ export function saveDuck (duck) {
 export function listenToFeed (cb, errorCB) {
   ref.child('ducks').on('value', (snapshot) => {
     const feed = snapshot.val() || {}
-    const sortedIds = Object.keys(feed).sort((a,b) => {
-      return feed[b].timestamp - feed[a].timestamp
-    })
+    const sortedIds = Object.keys(feed).sort((a, b) => feed[b].timestamp - feed[a].timestamp)
     cb({feed, sortedIds})
   }, errorCB)
+}
+
+export function fetchUsersLikes (uid) {
+  return ref.child(`usersLikes/${uid}`).once('value')
+    .then((snapshot) => snapshot.val() || {})
+}
+
+export function saveToUsersLikes (uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(true)
+}
+
+export function deleteFromUsersLikes (uid, duckId) {
+  return ref.child(`usersLikes/${uid}/${duckId}`).set(null)
+}
+
+export function incrementNumberOfLikes (duckId) {
+  return ref.child(`likeCount/${duckId}`)
+    .transaction((currentValue = 0) => currentValue + 1)
+}
+
+export function decrementNumberOfLikes (duckId) {
+  return ref.child(`likeCount/${duckId}`)
+    .transaction((currentValue = 0) => currentValue - 1)
 }
