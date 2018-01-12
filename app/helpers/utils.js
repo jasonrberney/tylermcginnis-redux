be45b32bf4a1bcae5532240fc164c79086c0702e
Jason Berney <jasonrberney@gmail.com>
2018-01-12 15:28:39 -0800

    Build out individual Duck.

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -1,17 +1,22 @@
-export function formatUserInfo (user, avatar, uid) {
+export function formatUserInfo (name, avatar, uid) {
     return {
-        name,
-        avatar,
-        uid,
+      name,
+      avatar,
+      uid,
     }
-}
-
-export function formatDuck (text, {name, avatar, uid}) {
+  }
+  
+  export function formatDuck (text, {name, avatar, uid}) {
     return {
-        text,
-        name,
-        avatar,
-        uid,
-        timestamp: Date.now()
+      text,
+      name,
+      avatar,
+      uid,
+      timestamp: Date.now(),
     }
-}
+  }
+  
+  export function formatTimestamp (timestamp) {
+    const date = new Date(timestamp)
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  }
